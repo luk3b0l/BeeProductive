1f58a124487e0b6372f851e80dd9e928ceb06983
luk3b0l <lukaszbol@yahoo.co.uk>
2017-10-03 15:30:28 +0100

    Added initial version of the menu. Added START and RESET buttons. Added individualised Action Listeners with separate Event Handlers classes

--- BeeProductive/src/beeproductive/UserGUI.java
+++ BeeProductive/src/beeproductive/UserGUI.java
@@ -6,39 +6,112 @@
 package beeproductive;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 /**
  *
  * @author xxx
  */
-public class UserGUI 
+public class UserGUI
 {
     private JFrame myFrame = new JFrame();
+    private JLabel helloLabel = new JLabel("Hello");
+    private JButton startButton = new JButton("Start");
+    private JButton resetButton = new JButton("Reset");
+
     
     public UserGUI()
     {
-        Container contentPane = myFrame.getContentPane();
-        contentPane.setLayout(new FlowLayout());
-        setMenuBar();
-
+        setFrame();
+    }
+    
+    private void setFrame()
+    {
+        setMenuBar(myFrame);
         
-        JLabel helloLabel = new JLabel("Hello");
+        Container contentPane = myFrame.getContentPane();
+        contentPane.setLayout(new FlowLayout());       
+        
         contentPane.add(helloLabel);
+        contentPane.add(startButton);
+        contentPane.add(resetButton);
+        
+        
+        // add INDIVIDUALISED LISTENERS (event handlers):
+        startButton.addActionListener(new startButtonHandler());
+        resetButton.addActionListener(new resetButtonHandler());
+
         myFrame.pack();
         myFrame.setVisible(true);
     }
     
-    public void setMenuBar()
+    private void setMenuBar(JFrame frame)
     {
         JMenuBar menubar = new JMenuBar();
-        myFrame.setJMenuBar(menubar);
+        frame.setJMenuBar(menubar);
+        
+        // *** FILE menu:
+        // create and add the File menu
         JMenu fileMenu = new JMenu("File");
         menubar.add(fileMenu);
-        JMenu aboutMenu = new JMenu("About");
-        menubar.add(aboutMenu);
         
         JMenuItem exitItem = new JMenuItem("Exit");
         fileMenu.add(exitItem);
+        exitItem.addActionListener(new ExitHandler());                           // TESTING!
+        // -----------------------------------------------
+        
+        // *** ABOUT menu:
+        JMenu helpMenu = new JMenu("Help");
+        menubar.add(helpMenu);
+        
+        JMenuItem howToItem = new JMenuItem("How to use");
+        helpMenu.add(howToItem);
+        howToItem.addActionListener(new howToUseHandler());
+        
+        JMenuItem aboutItem = new JMenuItem("About");
+        helpMenu.add(aboutItem);
+        aboutItem.addActionListener(new aboutHandler());
+        // -----------------------------------------------
+    }
+    
+    private class ExitHandler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            System.exit(0);
+        }
+    }
+    
+    private class howToUseHandler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            System.out.println("Give info on how to use the software");
+        }
+    }
+    
+    private class aboutHandler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            System.out.println("About section...");
+        }
+    }
+
+    private class startButtonHandler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            System.out.println("START button executed...");
+        }
+    }
+
+    private class resetButtonHandler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            System.out.println("RESET the settings...");
+        }
     }
 }
