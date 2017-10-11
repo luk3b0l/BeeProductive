aaf527a350cb69cbb7a04e9221b0e827a5ba9743
luk3b0l <lukaszbol@yahoo.co.uk>
2017-10-11 22:16:27 +0100

    Added UsersDatabase class to hold users' profiles. Used a SINGLETON pattern as only one instance of a users' database is needed. This reduces potential errors that may arise when creating and querying a completely new instance of a database.

---
+++ BeeProductive/src/beeproductive/UsersDatabase.java
@@ -0,0 +1,41 @@
+package beeproductive;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lukasz Bol
+ * Used Singleton pattern to create only one instance of the UsersDatabase class. 
+ * This is because we want to have only one instance of the users' database.
+ * The precedence will make sure that only one instance of the UserDatabase is accessed,
+ * which decreases any confusion or errors that may arise later on while using the application.
+ */
+public class UsersDatabase 
+{
+    private static UsersDatabase theDatabaseInstance;
+    private ArrayList<UserProfile> users;
+    
+    private UsersDatabase()
+    {
+        users = new ArrayList<>();
+    }
+    
+    public UsersDatabase getInstance()
+    {
+        if(theDatabaseInstance == null)
+        {
+            theDatabaseInstance = new UsersDatabase();
+        }
+        return theDatabaseInstance;
+    }
+    
+    public void addUser(UserProfile newUser)
+    {
+        users.add(newUser);
+    }         
+    
+    public void removeUser(UserProfile existingUser)
+    {
+        users.remove(existingUser);
+    }
+}
