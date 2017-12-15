5dbdd4e52a76fa359ba5572924e1a25e60d294c4
lukaszbol <lukaszbol@yahoo.co.uk>
2017-12-15 23:04:54 +0000

    Improved code readability by changing variables' names.

--- BeeProductive/src/beeproductive/gui/UserGUI.java
+++ BeeProductive/src/beeproductive/gui/UserGUI.java
@@ -22,47 +22,47 @@ import javax.swing.Timer;
 
 public class UserGUI
 {   
-    private boolean newIntervalTimer = false;
-    private boolean newBreakTimer = false;
+    private boolean timerNewInterval = false;
+    private boolean timerNewBreak = false;
     private Timer timer;
     private Timer breakTimer;
     
     private UserProfile user;
-    private JFrame myFrame = new JFrame("Bee Productive");
+    private JFrame mainGUIFrame = new JFrame("Bee Productive");
     private JPanel tempPanel;
 
-    private JLabel setInterval = new JLabel("Interval:");
-    private JLabel setBreak = new JLabel("Break:");
-    private JLabel setRepetitions = new JLabel("Repetitions: ");
-    private JButton newSessionButton = new JButton("New Session");
-    private JButton startButton = new JButton("Start");
-    private JButton resetButton = new JButton("Reset");
-    private JTextField countdownTimer = new JTextField("", JTextField.CENTER);
-    private JTextField intervalTimeInput = new JTextField("");
-    private JTextField breakTimeInput = new JTextField("");
-    private JTextField repetitionsInput = new JTextField("");
-    private JSlider intervalSlider = new JSlider(1, 90, 1);
-    private JSlider breakSlider = new JSlider(1, 30, 1);
-    private JSlider repetitionSlider = new JSlider(1, 10, 1);
+    private JLabel labelSetInterval = new JLabel("Interval:");
+    private JLabel labelSetBreak = new JLabel("Break:");
+    private JLabel labelSetRepetitiions = new JLabel("Repetitions: ");
+    private JButton buttonNewSession = new JButton("New Session");
+    private JButton buttonStart = new JButton("Start");
+    private JButton buttonReset = new JButton("Reset");
+    private JTextField outputCountdownTimer = new JTextField("", JTextField.CENTER);
+    private JTextField inputIntervalTime = new JTextField("");
+    private JTextField inputBreakTime = new JTextField("");
+    private JTextField inputRepetitions = new JTextField("");
+    private JSlider sliderInterval = new JSlider(1, 90, 1);
+    private JSlider sliderBreak = new JSlider(1, 30, 1);
+    private JSlider sliderRepetitions = new JSlider(1, 10, 1);
     
     public UserGUI()
     {
-        setFrame();
+        setGUIFrame();
     }
     
-    private void setFrame()
+    private void setGUIFrame()
     {        
-        Container contentPane = myFrame.getContentPane();
+        Container contentPane = mainGUIFrame.getContentPane();
         contentPane.setLayout(new BorderLayout());
-        Dimension preferredSize = new Dimension(350, 400);
-        myFrame.setPreferredSize(preferredSize);
-        setMenuBar(myFrame);
+        Dimension preferredWindowSize = new Dimension(350, 400);
+        mainGUIFrame.setPreferredSize(preferredWindowSize);
+        setMenuBar(mainGUIFrame);
        
         // ***** N O R T H
         JPanel northPanel = new JPanel();
         contentPane.add(northPanel, BorderLayout.NORTH);
         northPanel.setLayout(new FlowLayout());
-        northPanel.add(newSessionButton);
+        northPanel.add(buttonNewSession);
         
         // ***** C E N T E R
         JPanel centerPanel = new JPanel();
@@ -73,14 +73,14 @@ public class UserGUI
         
         gcCenter.anchor = GridBagConstraints.SOUTH;
         gcCenter.gridx = 0; gcCenter.gridy = 0;
-        centerPanel.add(countdownTimer, gcCenter);
-        countdownTimer.setEditable(false);
-        countdownTimer.setPreferredSize(new Dimension(160,100));
-        countdownTimer.setFont(new Font("Serif", Font.BOLD, 65));
+        centerPanel.add(outputCountdownTimer, gcCenter);
+        outputCountdownTimer.setEditable(false);
+        outputCountdownTimer.setPreferredSize(new Dimension(160,100));
+        outputCountdownTimer.setFont(new Font("Serif", Font.BOLD, 65));
         
         gcCenter.anchor = GridBagConstraints.CENTER;
         gcCenter.gridx = 0; gcCenter.gridy = 1;
-        centerPanel.add(startButton, gcCenter);
+        centerPanel.add(buttonStart, gcCenter);
         
         gcCenter.weighty = 7; 
         gcCenter.gridx = 0; gcCenter.gridy = 2;
@@ -97,86 +97,86 @@ public class UserGUI
         // COLUMN 1:
         gcSouth.anchor = GridBagConstraints.LINE_START;
         gcSouth.gridx = 0; gcSouth.gridy = 0;
-        southPanel.add(setInterval, gcSouth);
+        southPanel.add(labelSetInterval, gcSouth);
         
         gcSouth.gridx = 0; gcSouth.gridy = 1;
-        southPanel.add(setBreak, gcSouth);
+        southPanel.add(labelSetBreak, gcSouth);
         
         gcSouth.gridx = 0; gcSouth.gridy = 2;
-        southPanel.add(setRepetitions, gcSouth);
+        southPanel.add(labelSetRepetitiions, gcSouth);
         
         // COLUMN 2:
         gcSouth.anchor = GridBagConstraints.LINE_START;
         gcSouth.gridx = 1; gcSouth.gridy = 0;
-        southPanel.add(intervalTimeInput, gcSouth);
-        intervalTimeInput.setPreferredSize(new Dimension(25,25));
-        intervalTimeInput.setFont(new Font("Serif", Font.PLAIN, 20));
-        intervalTimeInput.setEnabled(false);
+        southPanel.add(inputIntervalTime, gcSouth);
+        inputIntervalTime.setPreferredSize(new Dimension(25,25));
+        inputIntervalTime.setFont(new Font("Serif", Font.PLAIN, 20));
+        inputIntervalTime.setEnabled(false);
         
         gcSouth.gridx = 1; gcSouth.gridy = 1;
-        southPanel.add(breakTimeInput, gcSouth);
-        breakTimeInput.setPreferredSize(new Dimension(25,25));
-        breakTimeInput.setFont(new Font("Serif", Font.PLAIN, 20));
-        breakTimeInput.setEnabled(false);
+        southPanel.add(inputBreakTime, gcSouth);
+        inputBreakTime.setPreferredSize(new Dimension(25,25));
+        inputBreakTime.setFont(new Font("Serif", Font.PLAIN, 20));
+        inputBreakTime.setEnabled(false);
 
         
         gcSouth.gridx = 1; gcSouth.gridy = 2;
-        southPanel.add(repetitionsInput, gcSouth);
-        repetitionsInput.setPreferredSize(new Dimension(25,25));
-        repetitionsInput.setFont(new Font("Serif", Font.PLAIN, 20));
-        repetitionsInput.setEnabled(false);
+        southPanel.add(inputRepetitions, gcSouth);
+        inputRepetitions.setPreferredSize(new Dimension(25,25));
+        inputRepetitions.setFont(new Font("Serif", Font.PLAIN, 20));
+        inputRepetitions.setEnabled(false);
         
         // COLUMN 3:
         gcSouth.anchor = GridBagConstraints.LINE_START;
         gcSouth.gridx = 2; gcSouth.gridy = 0;
-        southPanel.add(intervalSlider, gcSouth);
+        southPanel.add(sliderInterval, gcSouth);
         
         gcSouth.gridx = 2; gcSouth.gridy = 1;
-        southPanel.add(breakSlider, gcSouth);
+        southPanel.add(sliderBreak, gcSouth);
         
         gcSouth.gridx = 2; gcSouth.gridy = 2;
-        southPanel.add(repetitionSlider, gcSouth);
+        southPanel.add(sliderRepetitions, gcSouth);
 
 
         // ***** E A S T
         JPanel eastPanel = new JPanel();
         contentPane.add(eastPanel, BorderLayout.EAST);
         eastPanel.setLayout(new FlowLayout());
-        eastPanel.add(resetButton);
+        eastPanel.add(buttonReset);
         
         
         // ***** Adding INDIVIDUALISED LISTENERS (event handlers):
         
         // Menu:
-        startButton.addActionListener(new startButtonHandler());
-        resetButton.addActionListener(new resetButtonHandler());
-        newSessionButton.addActionListener(new newSessionButtonHandler());
-        intervalSlider.addChangeListener(new intervalSliderHandler());
-        breakSlider.addChangeListener(new breakSliderHandler());
-        repetitionSlider.addChangeListener(new repetitionSliderHandler());
+        buttonStart.addActionListener(new buttonStartHandler());
+        buttonReset.addActionListener(new buttonResetHandler());
+        buttonNewSession.addActionListener(new NewSessionButtonHandler());
+        sliderInterval.addChangeListener(new sliderIntervalHandler());
+        sliderBreak.addChangeListener(new sliderBreakHandler());
+        sliderRepetitions.addChangeListener(new sliderRepetitionsHandler());
 
         initialState();     // setting initial state of the application
-        myFrame.pack();
-        myFrame.setVisible(true);
-        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myFrame.setLocationRelativeTo(null);    // setting the program in the centre of the screen
+        mainGUIFrame.pack();
+        mainGUIFrame.setVisible(true);
+        mainGUIFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainGUIFrame.setLocationRelativeTo(null);    // setting the program in the centre of the screen
     }
     
     private void initialState()
     {
-            setInterval.setVisible(false);
-            setBreak.setVisible(false);
-            setRepetitions.setVisible(false);
-            newSessionButton.setVisible(true);
-            startButton.setVisible(false);
-            resetButton.setVisible(false);
-            countdownTimer.setVisible(false);    
-            intervalTimeInput.setVisible(false);        
-            breakTimeInput.setVisible(false);        
-            repetitionsInput.setVisible(false);        
-            intervalSlider.setVisible(false);        
-            breakSlider.setVisible(false);  
-            repetitionSlider.setVisible(false);
+            labelSetInterval.setVisible(false);
+            labelSetBreak.setVisible(false);
+            labelSetRepetitiions.setVisible(false);
+            buttonNewSession.setVisible(true);
+            buttonStart.setVisible(false);
+            buttonReset.setVisible(false);
+            outputCountdownTimer.setVisible(false);    
+            inputIntervalTime.setVisible(false);        
+            inputBreakTime.setVisible(false);        
+            inputRepetitions.setVisible(false);        
+            sliderInterval.setVisible(false);        
+            sliderBreak.setVisible(false);  
+            sliderRepetitions.setVisible(false);
     }
     
     private void setMenuBar(JFrame frame)
@@ -190,7 +190,7 @@ public class UserGUI
         
         JMenuItem newSessionItem = new JMenuItem("New session");
         fileMenu.add(newSessionItem);
-        newSessionItem.addActionListener(new newSessionButtonHandler());
+        newSessionItem.addActionListener(new NewSessionButtonHandler());
         
         JMenuItem exitItem = new JMenuItem("Exit");
         fileMenu.add(exitItem);
@@ -248,7 +248,7 @@ public class UserGUI
     // - New session
     // - Exit
     
-    private class newSessionButtonHandler implements ActionListener
+    private class NewSessionButtonHandler implements ActionListener
     {
         @Override
         public void actionPerformed(ActionEvent e) 
@@ -259,29 +259,26 @@ public class UserGUI
                 System.out.println("NAME: " + name);
                 setUserProfile(name);
                  
-                setInterval.setVisible(true);
-                setBreak.setVisible(true);
-                setRepetitions.setVisible(true);
-                newSessionButton.setVisible(false);
-                startButton.setVisible(true);
-                resetButton.setVisible(true);
-                countdownTimer.setVisible(true);    
-                intervalTimeInput.setVisible(true);        
-                breakTimeInput.setVisible(true);        
-                repetitionsInput.setVisible(true);        
-                intervalSlider.setVisible(true);        
-                breakSlider.setVisible(true);      
-                repetitionSlider.setVisible(true);
+                labelSetInterval.setVisible(true);
+                labelSetBreak.setVisible(true);
+                labelSetRepetitiions.setVisible(true);
+                buttonNewSession.setVisible(false);
+                buttonStart.setVisible(true);
+                buttonReset.setVisible(true);
+                outputCountdownTimer.setVisible(true);    
+                inputIntervalTime.setVisible(true);        
+                inputBreakTime.setVisible(true);        
+                inputRepetitions.setVisible(true);        
+                sliderInterval.setVisible(true);        
+                sliderBreak.setVisible(true);      
+                sliderRepetitions.setVisible(true);
                 JPanel tempPanel = getPanel();
                 tempPanel.setBorder(BorderFactory.createTitledBorder("Settings"));
             }
             else
             {
-                JOptionPane.showMessageDialog(myFrame, "No name added or name less than 3.", "ERROR Info", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(mainGUIFrame, "No name added or name less than 3.", "ERROR Info", JOptionPane.ERROR_MESSAGE);
             }
-            
-            
- 
         }
     }
     
@@ -289,7 +286,7 @@ public class UserGUI
     {
         public void actionPerformed(ActionEvent e)
         {
-            int answer = JOptionPane.showConfirmDialog(myFrame, "Are you sure you want to exit?", "Exit Confirmation", JOptionPane.YES_NO_OPTION); 
+            int answer = JOptionPane.showConfirmDialog(mainGUIFrame, "Are you sure you want to exit?", "Exit Confirmation", JOptionPane.YES_NO_OPTION); 
             if(answer == JOptionPane.YES_OPTION)
             {
                 System.exit(0);
@@ -366,7 +363,7 @@ public class UserGUI
                              + "2. set break (in which you will have time for anything you want)\n"
                              + "3. set how many repetitions (interval + break) you want to have\n"
                              + "4. Press START button and enjoy working!";
-            JOptionPane.showMessageDialog(myFrame, message, title, JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(mainGUIFrame, message, title, JOptionPane.INFORMATION_MESSAGE);
         }
     }
     
@@ -381,19 +378,19 @@ public class UserGUI
                             + "\n\nAuthor: Lukasz Bol"
                             + "\nContact: lukaszbol@yahoo.co.uk"
                             + "\nVersion: 1.1  [October 2017]";
-            JOptionPane.showMessageDialog(myFrame, message, title, JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(mainGUIFrame, message, title, JOptionPane.INFORMATION_MESSAGE);
         }
     }
     // ---------------------------------------------------------------------------------------------------
     
     // ***** BUTTON HANDLERS:
-    private class startButtonHandler implements ActionListener
+    private class buttonStartHandler implements ActionListener
     {
         public void actionPerformed(ActionEvent e)
         {
-            int intervalTime = intervalSlider.getValue();
-            int breakTime = breakSlider.getValue();
-            String repetitions = repetitionsInput.getText();
+            int intervalTime = sliderInterval.getValue();
+            int breakTime = sliderBreak.getValue();
+            String repetitions = inputRepetitions.getText();
             int repetitionsNum = Integer.parseInt(repetitions);     
             System.out.println("TEST 1");
 
@@ -442,7 +439,7 @@ public class UserGUI
             if(intervalToSeconds >= 0)
             {   
                 System.out.println("Check: interval");
-                countdownTimer.setText(finalMinutes + ":" + finalSeconds);           
+                outputCountdownTimer.setText(finalMinutes + ":" + finalSeconds);           
             }
             else
             {            
@@ -451,7 +448,7 @@ public class UserGUI
 
 
                 
-                int breakTime = breakSlider.getValue();
+                int breakTime = sliderBreak.getValue();
                 BreakTimeClass breakTimeCounter = new BreakTimeClass(breakTime);
                 breakTimer = new Timer(500, breakTimeCounter);
                 breakTimer.start();
@@ -488,7 +485,7 @@ public class UserGUI
             if(breakToSeconds >= 0)
             {
                 System.out.println("Check: break");
-                countdownTimer.setText(finalMinutes + ":" + finalSeconds);        
+                outputCountdownTimer.setText(finalMinutes + ":" + finalSeconds);        
             }
             else
             {
@@ -501,49 +498,46 @@ public class UserGUI
     
 
     
-    private class resetButtonHandler implements ActionListener
+    private class buttonResetHandler implements ActionListener
     {
         public void actionPerformed(ActionEvent e)
         {
-            intervalTimeInput.setText("0");
-            breakTimeInput.setText("0");
-            repetitionsInput.setText("0");
-            countdownTimer.setText("--:--");
+            inputIntervalTime.setText("0");
+            inputBreakTime.setText("0");
+            inputRepetitions.setText("0");
+            outputCountdownTimer.setText("--:--");
         }
     }
     // ---------------------------------------------------------------------------------------------------
 
     // ***** SLIDERS HANDLERS:
-    private class intervalSliderHandler implements ChangeListener
+    private class sliderIntervalHandler implements ChangeListener
     {
         @Override
         public void stateChanged(ChangeEvent e) 
         {
-            intervalTimeInput.setText("" + intervalSlider.getValue());
+            inputIntervalTime.setText("" + sliderInterval.getValue());
         }
     }
     
-    private class breakSliderHandler implements ChangeListener
+    private class sliderBreakHandler implements ChangeListener
     {
         @Override
         public void stateChanged(ChangeEvent e) 
         {
-            breakTimeInput.setText("" + breakSlider.getValue());
+            inputBreakTime.setText("" + sliderBreak.getValue());
         }
     }
     
-    private class repetitionSliderHandler implements ChangeListener
+    private class sliderRepetitionsHandler implements ChangeListener
     {
         @Override
         public void stateChanged(ChangeEvent e) 
         {
-            repetitionsInput.setText("" + repetitionSlider.getValue());
+            inputRepetitions.setText("" + sliderRepetitions.getValue());
         }
     }
     // ---------------------------------------------------------------------------------------------------
- 
-    // ***** Additional methods:
-    
     public void sendPanel(JPanel panel)
     {
         this.tempPanel = panel;
@@ -554,19 +548,13 @@ public class UserGUI
         return this.tempPanel;
     }
     
-    public void setUserProfile(String name)
+    public void setUserProfile(String newName)
     {
-        this.user = new UserProfile(name);
+        this.user = new UserProfile(newName);
     }
     
     public UserProfile getUserProfile()
     {
         return this.user;
     }    
-    // ---------------------------------------------------------------------------------------------------
-
-    public static void main(String [] args)
-    {
-        new UserGUI();
-    }
 }
